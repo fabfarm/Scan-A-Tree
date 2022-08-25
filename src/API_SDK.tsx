d4fb93f3f10b8204f818a999bef3c3501558dc13
Daniel Tchangang <daniel.tchangang@gmail.com>
2022-08-25 18:01:18 +0200

    feat: create API_SDK

---
+++ src/API_SDK.tsx
@@ -0,0 +1,35 @@
+const basicFetchService = {
+  get: <T,>(url: string): Promise<T> => {
+    return fetch(url).then((res) => res.json());
+  },
+  post: <T,>(url: string, body: Record<string, unknown>): Promise<T> => {
+    return fetch(url, { method: 'POST', body: JSON.stringify(body) }).then(
+      (res) => res.json(),
+    );
+  },
+};
+const ROUTES = {
+  DATA: '/api/data/data',
+  STATUSES: '/api/data/statuses',
+  UPDATE_STATUS: '/api/data/update_status',
+};
+
+type Status = {
+  name: string;
+  icon: string;
+};
+
+export const API_SDK = {
+  getData: () => {
+    return basicFetchService.get<Array<Record<string, string>>>(ROUTES.DATA);
+  },
+  getStatuses: () => {
+    return basicFetchService.get<Status[]>(ROUTES.STATUSES);
+  },
+  updateStatus: (itemId: string, newStatus: string) => {
+    return basicFetchService.post(ROUTES.STATUSES, {
+      id: itemId,
+      newStatus: newStatus,
+    });
+  },
+};
