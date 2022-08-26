ec8c355268180ab94d8eafd92b23e3d9b2bfa705
Daniel Tchangang <daniel.tchangang@gmail.com>
2022-08-26 09:59:14 +0200

    feat: update check page

--- pages/check.tsx
+++ pages/check.tsx
@@ -1,3 +1,64 @@
-export const CheckPage = () => {
-  return <div>Empty page</div>;
+import { useEffect, useState } from 'react';
+import { QrReader } from 'react-qr-reader';
+import { useAsyncFn } from 'react-use';
+import { API_SDK } from '../src/API_SDK';
+import { DataFields } from '../src/components/DataFields';
+
+const CheckPage = () => {
+  const [qrCodeValue, setQrCodeValue] = useState<string>('2');
+  const [dataState, getDataState] = useAsyncFn(API_SDK.getDataById);
+
+  useEffect(() => {
+    if (qrCodeValue) {
+      getDataState(qrCodeValue);
+    }
+  }, [qrCodeValue]);
+
+  if (dataState.loading) {
+    return <div>Loading</div>;
+  }
+  if (dataState.error) {
+    return <div>{dataState.error.message}</div>;
+  }
+  if (!qrCodeValue) {
+    return <div>Voluntarily off</div>;
+    return <QRBlock onChange={setQrCodeValue} />;
+  }
+
+  if (!dataState.value) {
+    return <div>No data found</div>;
+  }
+
+  const dataItem = dataState.value;
+
+  return (
+    <div>
+      <DataFields
+        {...(dataItem as unknown as Record<string, string>)}
+        column={'true'}
+      />
+    </div>
+  );
 };
+
+const QRBlock = ({ onChange }: { onChange: (textValue: string) => void }) => {
+  return (
+    <div style={{ width: 300, height: 300 }}>
+      <QrReader
+        onResult={(result, error) => {
+          if (!!result) {
+            onChange(result?.getText());
+          }
+
+          if (!!error) {
+            console.info(error);
+          }
+        }}
+        constraints={{}}
+        // style={{ width: '100%' }}
+      />
+    </div>
+  );
+};
+
+export default CheckPage;
--- src/components/DataFields.tsx
+++ src/components/DataFields.tsx
@@ -4,9 +4,14 @@ export const DataFields = ({
   age,
   comingFrom,
   status,
+  column,
 }: Record<string, string>) => {
   return (
-    <div key={id} className='data_container'>
+    <div
+      key={id}
+      className='data_container'
+      style={column ? { gridTemplateColumns: 'auto' } : {}}
+    >
       <span>Name: {name}</span>
       <span>Planted on: {age}</span>
       <span>Coming from: {comingFrom}</span>
