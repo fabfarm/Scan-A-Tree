bfbee2bf13afa84442517bb13235cfb204f16740
Daniel Tchangang <daniel.tchangang@gmail.com>
2022-08-29 11:50:30 +0200

    update sheet database url

--- server/sheetDatabase.ts
+++ server/sheetDatabase.ts
@@ -5,7 +5,7 @@ type SheetNames = 'data' | 'statuses';
 
 export const getSheetDatabase = (sheetName: SheetNames) => {
   return new Database({
-    db: '1cS_Hb5uSuXZiKC-VPyNzq7Lvu3YZqIgc3D1ARK5NVHo',
+    db: '1G3qRF1ijyU3yDs0fn9LFvjuuHOvYqqTLjLs2D95kXB0',
     table: sheetName, // optional, default = Sheet1
     keyFile: path.join(process.cwd(), 'scan-a-tree-e603d426671c.json'),
     cacheTimeoutMs: 5000, // optional, default = 5000
